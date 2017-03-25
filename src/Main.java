6d21bc6787f9bcb1a392b8a81803733c14185abf
hcheraghpoor <h.cheraghpoor@gmail.com>
2017-03-25 11:48:42 +0430

    نسخه اولیه
    تست

--- src/Main.java
+++ src/Main.java
@@ -9,10 +9,46 @@
  * @author mohammadHossein
  */
 public class Main {
-    public static void main(String [] args){
-        Point obj1 = new Point(); 
+
+    public static void main(String[] args) {
+        //testing constructors
+        System.out.println("testing constructors");
+        Point obj1 = new Point();
+        Point obj2 = new Point(89, -100);
+        System.out.println(obj1.getXY());
+        System.out.println(obj2.getXY());
+        ////////////////////////////////////////////////
+        //testing setters
+        System.out.println("testing setters");
+        obj1.setPoint(100, -100);
+        obj2.setX(40);
+        obj2.setY(-90);
+        Point obj3 = new Point();
+        obj3.setPoint(obj1);
+        System.out.println(obj1.getXY());
+        System.out.println(obj2.getXY());
+        System.out.println(obj3.getXY());
+        ////////////////////////////////////////////////
+        //testing getters
+        System.out.println("testing getters");
+        Point obj4 = new Point(80, 34);
+        System.out.println("x: " + obj4.getX());
+        System.out.println("y: " + obj4.getY());
+        System.out.println("(x,y): " + obj4.getXY());
+        ////////////////////////////////////////////////
+        //other Methdos
+        System.out.println("testing other Methods");
+        Point obj5 = new Point(5, 3);
+        Point obj6 = new Point(2, 7);
+        Point obj7 = new Point();
+        obj7.sum(obj5, obj6);
+        System.out.println("sum of obj5 , obj6 is: " + obj7.getXY());
+        obj7.minus(obj5, obj6);
+        System.out.println("minus of obj5 , obj6 is: " + obj7.getXY());
+        double distance;
+        distance = obj5.getDistance(obj6);
+        System.out.println("the distance between obj5 , obj6 is: " + distance);
+
     }
-   
-    
-    
+
 }
--- src/Point.java
+++ src/Point.java
@@ -18,9 +18,13 @@ public class Point {
     //############################ Methods
     //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ Constructors
     public Point() {
-
+        this.setPoint(0, 0);
     }
 
+    public Point(int xx, int yy) {
+        this.setPoint(xx, yy);
+    }
+    
     //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ Setters
     public void setPoint(int xx, int yy) {
         //-1000 و1000 فرض بر این است که طول و عرض هر نقطه بین 
@@ -30,32 +34,52 @@ public class Point {
         } else {
             this.x = 0;
             this.y = 0;
+            System.err.println("your x and y value`s must be in range of [-1000,1000]...");
         }
     }
     
-    public void setPoint(){
-         //-1000 و1000 فرض بر این است که طول و عرض هر نقطه بین 
-        this.setPoint(0, 0);
+    public void setX(int xx) {
+        this.setPoint(xx, this.y);
     }
-     public void setPoint(int xx){
-         this.setPoint(xx, this.y);
-     }
-//     public void setPoint(int yy){
-//         this.setPoint(this.x, yy);
-//     }
-     public void setPoint(Point p){
-         this.setPoint(this.x, this.y);
-     }
+
+    public void setY(int yy) {
+        this.setPoint(this.x, yy);
+    }
+
+    //the clone constructor
+    public void setPoint(Point p) {
+        this.x = p.x;
+        this.y = p.y;
+    }
+
     //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ Getters
-     public String getXY(){
-         return "("+ this.x + "," + this.y + ")";
-     }
-     public int getX(){
-         return this.x;
-     }
-     public int getY(){
-         return this.y;
-     }
+    public String getXY() {
+        return "(" + this.x + "," + this.y + ")";
+    }
+
+    public int getX() {
+        return this.x;
+    }
+
+    public int getY() {
+        return this.y;
+    }
     //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ Other methods
-     
+    public void sum(Point p1, Point p2){
+        int xx , yy;
+        xx = p1.x + p2.x;
+        yy = p1.y +p2.y;
+        this.setPoint(xx, yy);
+    }
+    public void minus(Point p1, Point p2){
+        int xx , yy;
+        xx = p1.x - p2.x;
+        yy = p1.y - p2.y;
+        this.setPoint(xx, yy);
+    }
+    public double getDistance(Point p){
+        double result = Math.pow(this.x - p.x, 2) + Math.pow(this.y - p.y, 2);
+        result = Math.sqrt(result);
+        return result;
+    }
 }
